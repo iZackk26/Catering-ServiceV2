c715a95d79632adcfca7d8ab9a5423728f682460
iZack <newxstorecr@gmail.com>
2024-04-21 14:20:08 -0600

    feat[master]: First base of project. After reset

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import Carousel from './components/Carrousel/Carrousel';
+import Carousel_Home from './components/Carrousel/Carrousel';
+
+function Home() {
+  return (
+    <div>
+      <Carousel_Home />
+    </div>
+    
+  )
+
+}
+
+export default Home;
---
+++ src/components/Carrousel/Carrousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Carousel } from 'flowbite-react';
+import img1 from '../../assets/Carousel/meat-food.png'
+import img2 from '../../assets/Carousel/seafood-image.png'
+import img3 from '../../assets/Carousel/traditional-food.jpg'
+
+function Carousel_Home() {
+  return (
+    <div className="relative mt-16 w-full h-screen">
+      {/* Carrusel */}
+      <Carousel>
+        <img src={img1} alt="Carne" className="w-full h-full object-cover" />
+        <img src={img2} alt="Mariscos" className="w-full h-full object-cover" />
+        <img src={img3} alt="Comida tradicional" className="w-full h-full object-cover" />
+      </Carousel>
+    </div>
+  );
+}
+
+export default Carousel_Home;
