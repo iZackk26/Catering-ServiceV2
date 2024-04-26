57cf43ee043efc7083a80d21f2bb84f14981020d
iZack <newxstorecr@gmail.com>
2024-04-25 22:31:49 -0600

    feat[navbar]: Adding new navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Components
 import { Navbar } from './Navabar'
+import { Link } from 'react-router-dom';
 
 
 // Assets
@@ -11,17 +12,21 @@ import logo from '../assets/logo.jpg'
 
 export default function Header() {
   return (
-    <header> 
-      <div className='top-0 w-full h-full z-10 flex flex-col justify-between'>
-        <div className="flex justify-between items-center p-4">
-          <div className="flex items-center ">
-            <div className="mr-4">
-              <img src={logo} alt="Logo" className="h-8" />
-            </div>
-            <h1 className="text-3xl">ROYAL CATERING</h1>
-          </div>
-          <Navbar />
+    <header>
+      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto'>
+        <Link to='/'>
+          <img src={logo} alt='logo' className='w-20 h-20' />
+        </Link>
+        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+          <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
         </div>
+        <Navbar />
       </div>
     </header>
   );
