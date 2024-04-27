49c0ce0ae98074e5da2db5c993fd92eeaffa84c4
iZack <newxstorecr@gmail.com>
2024-04-27 09:31:32 -0600

    feat: Packages

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -194,7 +194,7 @@ export default function Packages() {
   return (
     <>
     <Header />
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+    <div className='px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
       {cardsData.map((card, index) => (
         <CardPackages key={index} {...card} />
       ))}
