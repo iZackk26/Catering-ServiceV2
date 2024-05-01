7ddfe31cef4e3f66fe702b7a57a5b5c20995d65d
Hector <hcaravacavargas@gmail.com>
2024-05-01 09:07:59 -0600

    feat: Adding the path of the about us page

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
 import Packages from './pages/Packages';
+import About from './pages/About';
 
 // Formulario de Catering
 import CateringForm from './components/contact/CateringForm';
@@ -16,6 +17,7 @@ const Routing = () => (
         <Route path="/packages" element={<Packages />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/catering" element={<CateringForm />} />
+        <Route path="/about" element={<About />} />
         </Routes>
     </Router>
 
